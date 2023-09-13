36fb7f285035728b6f5c5d1ee1f8cd9e1284614e
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-09-14 00:03:12 +0800

    tailwind integrated

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      'black': "#000",
+      'white': "#fff",
+      'primary': '#1E1E1E',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
